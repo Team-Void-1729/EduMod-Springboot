1497a39ec00cd91cc7c0a29a8baae31616b6bd8d
Aratrik123 <aratrik.bandyopadhyay.21@aot.edu.in>
2023-12-19 11:00:52 +0530

    update

--- edumoduler/src/main/java/org/aot/edumoduler/EdumodulerApplication.java
+++ edumoduler/src/main/java/org/aot/edumoduler/EdumodulerApplication.java
@@ -1,6 +1,5 @@
 package org.aot.edumoduler;
 
-import org.aot.edumoduler.git.VersionControl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,13 +9,6 @@ public class EdumodulerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EdumodulerApplication.class, args);
 		System.out.println("Application started....");	
-		
-//		try {
-//			VersionControl versionControl = new VersionControl();
-//			versionControl.cloneRepo();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
 	}
 
 }
